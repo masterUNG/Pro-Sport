8d543a5ae9abd1f28d89ee1a7a41e2d613f61c6b
masterUNG <phrombutr@gmail.com>
2016-08-11 15:04:58 +0700

    Check Space finish

--- app/src/main/java/appewtc/masterung/prosport/MainActivity.java
+++ app/src/main/java/appewtc/masterung/prosport/MainActivity.java
@@ -4,14 +4,47 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
     }   // Main Method
+
+    public void clickSignIn(View view) {
+
+        //Get Value
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,
+                    getResources().getString(R.string.have_space_title),
+                    getResources().getString(R.string.have_space_message));
+        } else {
+            //No Space
+
+        }
+
+    }   // clickSignIn
+
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
