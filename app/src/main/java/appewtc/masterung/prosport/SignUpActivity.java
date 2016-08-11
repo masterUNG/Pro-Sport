e15010e750cb4a52ec57b2f12e6ee50edf71b5f4
masterUNG <phrombutr@gmail.com>
2016-08-11 14:20:10 +0700

    Post New User to mySQL

--- app/src/main/java/appewtc/masterung/prosport/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/prosport/SignUpActivity.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
     private EditText nameEditText, userEditText, passwordEditText;
     private String nameString, userString, passwordString;
+    private static final String urlPHP = "http://swiftcodingthai.com/mama/add_user_master.php";
 
 
     @Override
@@ -39,9 +50,36 @@ public class SignUpActivity extends AppCompatActivity {
             myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
         } else {
             //No Space
-
+            uploadNewUserToServer();
         }
 
     }   // clickSign
+
+    private void uploadNewUserToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+
+    }   // upload
 
 }   // Main Class
