4f5a16c1f43e1b3b76c64d169d37d5750f2a46c5
masterUNG <phrombutr@gmail.com>
2016-08-12 10:54:21 +0700

    Intent to Get Content

--- app/src/main/java/appewtc/masterung/prosport/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/prosport/ServiceActivity.java
@@ -1,13 +1,35 @@
 package appewtc.masterung.prosport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private static final int myINT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+    }   // Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
-}
+
+    public void clickUploadVideo(View view) {
+
+        //Choose Video
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent
+                , "Select Picture"), myINT);
+
+
+    }   // upload
+
+}   // Main Class
